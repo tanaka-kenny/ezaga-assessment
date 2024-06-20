b1ab5dca3d5844259afd009cf16e642e9aceb84f
Tanaka Madondo <tanakam@fingerprintsoft.org>
2024-06-20 11:06:01 +0200

    Add register ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         'path': 'login',
         loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        'path': 'register',
+        loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
     }
 ];
