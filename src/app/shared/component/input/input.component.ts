77830141f472031e0362d2d4d3fa0b6c02b063fa
Tanaka Madondo <tanakam@fingerprintsoft.org>
2024-06-22 11:48:30 +0200

    Add error validation on forms

--- src/app/shared/component/input/input.component.ts
+++ src/app/shared/component/input/input.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf, NgFor, CommonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
-export class InputComponent {
+export class InputComponent implements OnInit {
 
   @Input({ required: true })
     formGroup!: FormGroup;
@@ -17,5 +18,18 @@ export class InputComponent {
   @Input() type = 'text'
   @Input({ required: true}) label!: string;
   @Input() readOnly = false;
+
+  errorMessages!: Record<string, string>;
+
+  ngOnInit(): void {
+    this.errorMessages = {
+      required: this.control.charAt(0).toUpperCase() + this.control.slice(1) + ' is required',
+      email: this.control.charAt(0).toUpperCase() + this.control.slice(1) + ' must be a valid email'
+    }
+  }
+
+  get formGroupControls() {
+    return this.formGroup.controls;
+  }
 
 }
